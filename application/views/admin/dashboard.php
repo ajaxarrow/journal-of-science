13b94aeb955b31675e96144d3bed601c758d0a57
ajaxarrow <ajaxarrow463@gmail.com>
2024-04-19 21:56:01 +0800

    topbar created and style for main content container

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,1 +1,9 @@
-<?php $this->load->view('./components/admin/admin-sidebar.php'); ?>
+<?php
+	$data['title'] = 'Admin Dashboard';
+	$this->load->view('./components/admin/admin-sidebar.php');
+	$this->load->view('./components/admin/admin-topbar.php', $data);
+?>
+
+<div class="main-content-container">
+
+</div>
