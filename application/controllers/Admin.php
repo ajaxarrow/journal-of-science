9b00a76eb5d864289b2c78f30d03dd27051e0464
ajaxarrow <ajaxarrow463@gmail.com>
2024-04-23 21:23:57 +0800

    fix bug in deleteing articles

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -10,42 +10,6 @@ class Admin extends CI_Controller {
 		$this->load->view('templates/footer');
 	}
 
-	public function users()
-	{
-		$data['users'] = $this->user_model->get_users();
-		$this->load->view('templates/header');
-		$this->load->view('admin/users', $data);
-		$this->load->view('templates/footer');
-	}
-
-	public function articles()
-	{
-		$this->load->view('templates/header');
-		$this->load->view('admin/articles');
-		$this->load->view('templates/footer');
-	}
-
-	public function submissions()
-	{
-		$this->load->view('templates/header');
-		$this->load->view('admin/submissions');
-		$this->load->view('templates/footer');
-	}
-
-	public function authors()
-	{
-		$this->load->view('templates/header');
-		$this->load->view('admin/authors');
-		$this->load->view('templates/footer');
-	}
-
-	public function volumes()
-	{
-		$this->load->view('templates/header');
-		$this->load->view('admin/volumes');
-		$this->load->view('templates/footer');
-	}
-
 
 }
 
